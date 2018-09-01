ea83ca681f72124fa3d5ab35452ce8ae5bce64b3
EliuX <eliux@EliuXs-Mac-mini.local>
2018-08-31 19:16:52 -0500

    First commit with basic swagger impl

---
+++ README.md
@@ -0,0 +1,8 @@
+BPM Projects API
+================
+
+This is the API project for BPM Projects
+
+
+# API
+The REST API contained in this project is built on Flask and Flask-RESTPlus
---
+++ bpm-projects-api/app.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask_restplus import Api, fields, Resource
+from werkzeug.contrib.fixers import ProxyFix
+
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='0.0.1',
+          title='BPM Projects API',
+          description='A simple TodoMVC API')
+
+ns = api.namespace('projects', description='Project operations')
+
+# Use to marshall and unmarshall responses and requests
+project = api.model('Project', {
+    'guid': fields.Integer(readOnly=True, title='Identifier', description='The project generated unique identifier'),
+    'short_name': fields.String(required=True, title='Short name', description='The task details'),
+    'comments': fields.String(title='Comments', description='Comments about the project'),
+    'properties_table': fields.String(title='Project table', description='Metadata for this project'),
+    'active': fields.Boolean(title='Is active?', description='Whether the project is active or not'),
+})
+
+
+class ProjectDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.projects = []
+
+    def get(self, id):
+        for project in self.projects:
+            if project['id'] == id:
+                return project
+        api.abort(404, "The project {} doesn't exist".format(id))
+
+    def create(self, project):
+        project['id'] = self.counter = self.counter + 1
+        self.projects.append(project)
+        return project
+
+    def update(self, id, data):
+        project = self.get(id)
+        project.update(data)
+        return project
+
+    def delete(self, id):
+        project = self.get(id)
+        self.projects.remove(project)
+
+
+dao = ProjectDAO()
+
+
+@ns.route('/')
+class ProjectList(Resource):
+    """Shows a list of all projects"""
+
+    @ns.doc('list_projects')
+    @ns.marshal_list_with(project)
+    def get(self):
+        """List all projects"""
+        return dao.projects
+
+    @ns.doc('create_project')
+    @ns.expect(project)
+    @ns.marshal_with(project, code=201)
+    def post(self):
+        """Create a project"""
+        return dao.create(api.payload), 201
+
+
+@ns.route('/<int:guid>')
+@ns.response(404, 'Project not found')
+@ns.param('guid', 'The project identifier')
+class Project(Resource):
+    """To show a project or delete it"""
+
+    @ns.doc('get_project')
+    @ns.marshal_with(project)
+    def get(self, id):
+        """Fetch a given project"""
+        return dao.get(id)
+
+    @ns.doc('delete_project')
+    @ns.response(204, 'Project deleted')
+    def delete(self, id):
+        """Delete a project given its identifier"""
+        dao.delete(id)
+        return None, 204
+
+    @ns.expect(project)
+    @ns.marshal_with(project)
+    def put(self, id):
+        """Update a project given its identifier"""
+        return dao.update(id, api.payload)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
