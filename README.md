adab54cf926c9f1cc90a86749eb461c60cebe655
EliuX <eliux@EliuXs-Mac-mini.local>
2018-09-03 18:40:01 -0500

    Fix README.md and enpoint for projects

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
 BPM Projects API
 ================
 
-This is the API project for BPM Projects
+API for BPM Projects
+
+## Getting started
+Follow the following instructions to get the project ready to use ASAP:
+
+### Requirements
+Be sure you have installed in your system
+
+- [Python version 3](https://www.python.org/download/releases/3.0/) in your path. It will install
+automatically [pip](https://pip.pypa.io/en/stable/) as well.
+- A virtual environment, namely [venv](https://docs.python.org/3/library/venv.html).
+
+### Install
+
+Thanks to the `requirements.txt` we can install the dependencies using 
+
+```bash
+ pip install -r requirements.txt
+```
+
+### Usage
+
+Run the project using 
+
+1. For using Flask's development capabilities as the autoloading set this `FLASK_ENV` environment variable
+to `development`, i.e.
+
+```bash
+export FLASK_ENV=development
+```
+
+1. Run the app from your terminal, i.e.
+
+```
+python bpm-projects-api/app.py
+```
+
+The use of an IDE is highly recommended, namely PyCharm.
+
+## Built with
+- [Python version 3](https://www.python.org/download/releases/3.0/) as backend programming language
+- [Flask](http://flask.pocoo.org/) as backend framework
+- [Flask RestPlus](https://flask-restplus.readthedocs.io/en/stable/) for building Restful APIs
+- [Swagger](https://swagger.io/) for documentation and standardization 
 
 
-# API
-The REST API contained in this project is built on Flask and Flask-RESTPlus
+## License
+
+IOET Inc. all rights reserved.
--- bpm-projects-api/app.py
+++ bpm-projects-api/app.py
@@ -2,21 +2,25 @@ from flask import Flask
 from flask_restplus import Api, fields, Resource
 from werkzeug.contrib.fixers import ProxyFix
 
-
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app, version='0.0.1',
-          title='BPM Projects API',
-          description='A simple TodoMVC API')
+          title='BPM API',
+          description='API for BPM Projects')
 
 ns = api.namespace('projects', description='Project operations')
 
-# Use to marshall and unmarshall responses and requests
+property = api.model('Property', {
+    'id': fields.String,
+    'content': fields.String
+})
+
+# Project model to marshall and unmarshall responses and requests
 project = api.model('Project', {
     'guid': fields.Integer(readOnly=True, title='Identifier', description='The project generated unique identifier'),
     'short_name': fields.String(required=True, title='Short name', description='The task details'),
     'comments': fields.String(title='Comments', description='Comments about the project'),
-    'properties_table': fields.String(title='Project table', description='Metadata for this project'),
+    'properties_table': fields.List(fields.Nested(property)),
     'active': fields.Boolean(title='Is active?', description='Whether the project is active or not'),
 })
 
@@ -28,12 +32,12 @@ class ProjectDAO(object):
 
     def get(self, id):
         for project in self.projects:
-            if project['id'] == id:
+            if project['guid'] == id:
                 return project
         api.abort(404, "The project {} doesn't exist".format(id))
 
     def create(self, project):
-        project['id'] = self.counter = self.counter + 1
+        project['guid'] = self.counter = self.counter + 1
         self.projects.append(project)
         return project
 
@@ -51,7 +55,7 @@ dao = ProjectDAO()
 
 
 @ns.route('/')
-class ProjectList(Resource):
+class Projects(Resource):
     """Shows a list of all projects"""
 
     @ns.doc('list_projects')
@@ -76,22 +80,22 @@ class Project(Resource):
 
     @ns.doc('get_project')
     @ns.marshal_with(project)
-    def get(self, id):
+    def get(self, guid):
         """Fetch a given project"""
-        return dao.get(id)
+        return dao.get(guid)
 
     @ns.doc('delete_project')
     @ns.response(204, 'Project deleted')
-    def delete(self, id):
+    def delete(self, guid):
         """Delete a project given its identifier"""
-        dao.delete(id)
+        dao.delete(guid)
         return None, 204
 
     @ns.expect(project)
     @ns.marshal_with(project)
-    def put(self, id):
+    def put(self, guid):
         """Update a project given its identifier"""
-        return dao.update(id, api.payload)
+        return dao.update(guid, api.payload)
 
 
 if __name__ == '__main__':
