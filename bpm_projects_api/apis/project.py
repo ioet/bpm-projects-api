525a3b2c64cf1258794e801d240addf5648ea662
EliuX <ehernandez@ioet.com>
2018-10-01 10:57:52 -0500

    Restoring the search and activate feature

--- bpm_projects_api/apis/project.py
+++ bpm_projects_api/apis/project.py
@@ -25,6 +25,14 @@ project = ns.model('Project', {
                                          'or not')
 })
 
+# Search model
+search_model = ns.model('SearchCriteria', {
+    'search_string': fields.String(title='Keywords',
+                                   description='What you want to search for in the comments/the name'),
+    'active': fields.Boolean(title='Is active?',
+                             description='true=only active, false=only inactive, none=all'),
+})
+
 
 class ProjectDAO(object):
     def __init__(self):
@@ -77,7 +85,7 @@ class Projects(Resource):
         return dao.create(ns.payload), 201
 
 
-@ns.route('/<uid>')
+@ns.route('/<string:uid>')
 @ns.response(404, 'Project not found')
 @ns.param('uid', 'The project identifier')
 @ns.doc()
@@ -105,3 +113,46 @@ class Project(Resource):
     def put(self, uid):
         """Update a project given its identifier"""
         return dao.update(uid, ns.payload)
+
+
+@ns.route('/search/')
+@ns.response(404, 'Project not found')
+class SearchProject(Resource):
+    """To search for projects"""
+
+    @ns.doc('search_project')
+    @ns.expect(search_model)
+    @ns.marshal_list_with(project, code=200)
+    def post(self):
+        """Fetch projects given a string"""
+        return dao.search(ns.payload)
+
+
+@ns.route('/status/activate/<string:uid>')
+@ns.response(404, 'Project not found')
+@ns.param('uid', 'The project identifier')
+class ActivateProject(Resource):
+    """To set a project active"""
+
+    @ns.doc('set_project_active')
+    @token_policies.administrator_required
+    @ns.marshal_list_with(project, code=200)
+    @ns.response(205, 'Project status changed')
+    def patch(self, uid):
+        """Set projects active given a string"""
+        return dao.activate(uid)
+
+
+@ns.route('/status/deactivate/<string:uid>')
+@ns.response(404, 'Project not found')
+@ns.param('uid', 'The project identifier')
+class DeactivateProject(Resource):
+    """To set a project inactive"""
+
+    @ns.doc('set_project_inactive')
+    @token_policies.administrator_required
+    @ns.marshal_list_with(project, code=200)
+    @ns.response(205, 'Project status changed')
+    def patch(self, uid):
+        """Set projects inactive given a string"""
+        return dao.deactivate(uid)
