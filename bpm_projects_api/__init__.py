99931c2c6fe3dc668e90201a7e56165df151b13c
EliuX <ehernandez@ioet.com>
2018-10-04 10:36:08 -0500

    Step down create_app

--- bpm_projects_api/__init__.py
+++ bpm_projects_api/__init__.py
@@ -4,13 +4,22 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 
 
-def create_app(config_path='bpm_projects_api.config.DevelopmentConfig'):
+def create_app(config_path='bpm_projects_api.config.DevelopmentConfig',
+               config_data=None):
     """Create and configure an instance of the Flask app."""
     app = Flask(__name__, instance_relative_config=True)
 
     # Needed when using a wsgi server (mainly for production)
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
+    init_app_config(app, config_path, config_data)
+
+    init_app(app)
+
+    return app
+
+
+def init_app_config(app, config_path, config_data):
     if config_path is not None:
         app.config.from_object(config_path)
     else:
@@ -23,6 +32,11 @@ def create_app(config_path='bpm_projects_api.config.DevelopmentConfig'):
         # Located in `/instance`
         app.config.from_pyfile('config.py', silent=True)
 
+    if config_data:
+        app.config.update(config_data)
+
+
+def init_app(app):
     import bpm_projects_api.model as model
     model.init_app(app)
 
@@ -31,5 +45,3 @@ def create_app(config_path='bpm_projects_api.config.DevelopmentConfig'):
 
     from bpm_projects_api.core import security
     app.register_blueprint(security.ns)  # Security endpoints
-
-    return app
