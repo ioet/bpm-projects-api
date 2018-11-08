070895bea1c3a4988841f259eec327724e0ec779
datmau <alex.cam.mol@gmail.com>
2018-11-08 16:21:56 -0500

    remove all securitys to end points

--- tests/utils.py
+++ tests/utils.py
@@ -8,18 +8,6 @@ def encodeBase64(str: str):
     return base64.b64encode(str.encode("UTF8")).decode()
 
 
-def open_with_basic_auth(client, login_url, username, password, method='GET'):
-    """ Authenticates in a login_url using Basic Authentication """
-    return client.open(login_url,
-                       method=method,
-                       headers={
-                           'Authorization': "Basic %s" % encodeBase64(
-                               "{}:{}".format(username, password)
-                           )
-                       }
-                       )
-
-
 def url_for(url, app):
     """Allows to use flask.url_for in the test context"""
     with app.app_context():
